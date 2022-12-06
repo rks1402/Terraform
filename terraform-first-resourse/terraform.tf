ec663b2dc84b9d46edae2ff7c5ce3b19972cd6a4
Rahul Kumar Sharma <rahulkumarsharma1402@gmail.com>
2022-12-07 01:02:26 +0530

    added comments in terraform.tf

--- terraform-first-resourse/terraform.tf
+++ terraform-first-resourse/terraform.tf
@@ -1,7 +1,10 @@
+//use "terraform init"  for downloading the plugin [in this case plugin of github] 
+
 provider "github" {
     token = "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"
 }
 
+//use "terraform apply"  for applying the code and making the infrastructure [in this case making the github repository]
 
 resource "github_repository" "terraform-first-repo" {
   name        = "first-repo-from-terraform"
