07af084df4358055c6a4ecf5fb908d0d09cdd18f
Rahul Kumar Sharma <rahulkumarsharma1402@gmail.com>
2022-12-22 17:56:20 +0530

    function code file added

---
+++ datastore/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file(var.credentials_file)
+
+  project = var.project
+  region  = var.region
+  zone    = var.zone
+}
+
+resource "google_storage_bucket" "excel-file-bucket" {
+  name          = "excel-file-bucket-007"
+  location      = "US"
+  force_destroy = true
+
+}
+
+
+
---
+++ datastore/terraform.tfvars
@@ -0,0 +1,3 @@
+project = "bucket-function-datastore"
+
+credentials_file = "C:\\Users\\Rahul\\Downloads\\bucket-function-datastore-f2002c256cb0.json"
